75ccf7d95760b31ff9f910961b9c666716a4e5d1
Koala Yeung <koalay@gmail.com>
2024-08-06 14:56:52 +0800

    main.js: debug issue with the early return
    * Use clousure to make sense of the early return.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,18 @@
-/* "Animate" the image on the home page */
-const home_image = document.getElementById("cangjie_home");
-if (!home_image) {
-    return;
-}
+(function () {
+    // "Animate" the image on the home page
+    const home_image = document.getElementById("cangjie_home");
+    if (!home_image) {
+        return;
+    }
 
-// Preload "cangjie-with-codes.png", presuming the default image is "cangjie-without-codes.png"
-const image = new Image();
-image.src = home_image.replace("cangjie-without-codes.png", "cangjie-with-codes.png");
+    // Preload "cangjie-with-codes.png", presuming the default image is "cangjie-without-codes.png"
+    const image = new Image();
+    image.src = home_image.replace("cangjie-without-codes.png", "cangjie-with-codes.png");
 
-home_image.addEventListener("mouseover", function(event) {
-    event.target.src.replace("cangjie-without-codes.png", "cangjie-with-codes.png");
-});
-home_image.addEventListener("mouseout", function(event) {
-    event.target.src.replace("cangjie-with-codes.png", "cangjie-without-codes.png");
-});
+    home_image.addEventListener("mouseover", function(event) {
+        event.target.src.replace("cangjie-without-codes.png", "cangjie-with-codes.png");
+    });
+    home_image.addEventListener("mouseout", function(event) {
+        event.target.src.replace("cangjie-with-codes.png", "cangjie-without-codes.png");
+    });
+})();
