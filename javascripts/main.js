a6aea7a06316242f970bed1c8da6ecbb84d71758
Koala Yeung <koalay@gmail.com>
2024-08-06 14:36:06 +0800

    Moved javascripts to main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+/* "Animate" the image on the home page */
+const home_image = document.getElementById("cangjie_home");
+if (!home_image) {
+    return;
+}
+
+// Preload "cangjie-with-codes.png", presuming the default image is "cangjie-without-codes.png"
+const image = new Image();
+image.src = home_image.replace("cangjie-without-codes.png", "cangjie-with-codes.png");
+
+home_image.addEventListener("mouseover", function(event) {
+    event.target.src.replace("cangjie-without-codes.png", "cangjie-with-codes.png");
+});
+home_image.addEventListener("mouseout", function(event) {
+    event.target.src.replace("cangjie-with-codes.png", "cangjie-without-codes.png");
+});
